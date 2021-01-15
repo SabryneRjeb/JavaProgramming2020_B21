d6748c5deab93b0db8bd8ed377fae49c14ce6eb2
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-15 17:46:05 -0500

    my intellij

--- src/day44_Inheritance/ShapeTask/Shape.java
+++ src/day44_Inheritance/ShapeTask/Shape.java
@@ -1,6 +1,43 @@
 package day44_Inheritance.ShapeTask;
 
-public class Shape {
+    public class Shape { // this is a super class something common for every shape
+    public String name;
+    public final static boolean isShape, hasArea, hasPerimeter;
+
+        public Shape(String name) {
+            this.name= name;
+        }
+        // final is used to not change the variables anymore to make the method constant
 
 
-}
+    static{
+        isShape = true;
+        hasArea= true;
+        hasPerimeter = true;
+    }
+
+    public double calcArea(){
+        return 0;
+    }
+    public double calcPerimeter(){
+        return 0;
+    }
+
+        @Override
+        public String toString() {
+            return "Shape{" +
+                    "name='" + name + '\'' +
+                    "Area ='"+calcArea()+'\''+
+                    "Perimeter = '"+ calcPerimeter()+'\''+
+                    '}';
+        }
+    }
+/*
+     1. create a class called Shape
+                        variables: name, isShape(boolean), hasArea(boolean), hasPerimeter(boolean)
+                        methods: setInfo, calcArea(), calcPerimeter(),
+                                 toString(): returns the name, area and perimeter that are returned by calcArea(), calcPerimeter()
+
+ */
+
+
